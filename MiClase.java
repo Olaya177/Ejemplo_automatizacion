de57af2b0f24f4cdf8d4f86747dab94c8d4e4530
Olaya177 <olayasamuel17@gmail.com>
2025-05-14 10:34:22 -0500

    Cambio 2

--- MiClase.java
+++ MiClase.java
@@ -1,7 +1,5 @@
 public class MiClase {
     public static void main(String[] args) {
         System.out.println("Hola, mundo!");
-        System.out.println("Cambiando el mundo");
     }
-}// comentario para prueba
-// comentario para prueba
+}
