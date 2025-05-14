6783dbc98f57855a09618466cf3c00e77b0b10f7
Olaya177 <olayasamuel17@gmail.com>
2025-05-14 10:42:49 -0500

    Cambio 5

--- MiClase.java
+++ MiClase.java
@@ -1,5 +1,6 @@
 public class MiClase {
     public static void main(String[] args) {
         System.out.println("Hola, mundo!");
+        System.out.println("Este es un programa de ejemplo.");
     }
 }
